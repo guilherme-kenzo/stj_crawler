3806810b87c98130190d5a7e55d81643d97c37c6
guilherme <guikenzo.s@gmail,com>
2018-04-21 09:50:23 -0300

    almost there

--- STJ_crawler.py
+++ STJ_crawler.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from sys import argv
-import csv
+import pandas as pd
+import selenium.webdriver.support.ui as ui
+from selenium.webdriver.common.keys import Keys
 import re
-import json
 
 def write_csv(file_name, content):
     with open(file_name, 'a') as f:
@@ -41,18 +45,34 @@ class STJ(object):
         supradoclist = self.driver.find_element_by_id('listadocumentos')
         doclist = self.driver.find_elements_by_tag_name('div')
         linkre = re.compile('(?<=\(\').+(?=\'\))')
+        counter = 0
+        df = pd.DataFrame()
         for i in doclist:
-            raw_link = i.find_element_by_xpath('//*[@title="Exibir a íntegra do acórdão."]').get_attribute('href')
-            link = linkre.search(raw_link).group()
-            titulos = [j.text for j in i.find_elements_by_class_name('docTitulo')]
-            conteudo = [j.text for j in i.find_elements_by_class_name('docTexto')]
-            if (len(titulos) == len(conteudo)):
-                tmp_dict = {titulos[i]:conteudo[i] for i in range(0, len(titulos))}
-                tmp_dict['link'] = 'http://www.stj.jus.br' + link
-                with open('data.txt', 'a') as f:
-                    json.dump(tmp_dict, f)
-                    f.write('\n')
-        # write_csv(argv[1], rows)
+            counter += 1
+            i.find_element_by_xpath('//*[@title="Exibir a íntegra do acórdão."]').click()#.get_attribute('href')
+            windows = self.driver.window_handles
+            self.driver.switch_to_window(windows[1])
+            element = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.ID, "id_formato_html"))
+            )
+            element.click()
+            arvore = self.driver.find_element_by_class_name('arvore_documentos')
+            name = arvore.find_element_by_tag_name('a').text
+            arvore.find_element_by_tag_name('a').click()
+            handles = self.driver.window_handles
+            popup_handle = handles[-1]
+            print(popup_handle)
+            self.driver.switch_to_window(popup_handle)
+            print(self.driver.page_source)
+            # link = 'http://www.stj.jus.br' + linkre.search(raw_link).group()
+            #
+            # titulos = [j.text for j in i.find_elements_by_class_name('docTitulo')]
+            # conteudo = [j.text for j in i.find_elements_by_class_name('docTexto')]
+            # if (len(titulos) == len(conteudo)):
+            #     tmp_dict['link'] = 'http://www.stj.jus.br' + link
+            #     tmp_dict = {titulos[i]:conteudo[i] for i in range(0, len(titulos))}
+                # print(tmp_dict['Ementa'])
+    # write_csv(argv[1], rows)
 
     def next_page(self):
         self.driver.find_element_by_class_name('iconeProximaPagina').click()
@@ -65,7 +85,7 @@ class STJ(object):
             except NoSuchElementException as e:
                 print(e)
                 self.driver.quit()
-            except:
-                self.auto_get()
+            # except:
+            #     self.auto_get()
 
-STJ('a').auto_get()
+STJ('civil').auto_get()
